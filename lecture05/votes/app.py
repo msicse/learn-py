d3ff611eb597dc86ca3a7c85b59b248fc0f4752b
Sumon <eubsumon@gmail.com>
2020-03-19 17:15:55 +0600

    updated-evening-19-03-2020

--- lecture05/votes/app.py
+++ lecture05/votes/app.py
@@ -1,12 +1,45 @@
 import requests
 from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 socketio = SocketIO(app)
 
+# start vote 0 section
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# @socketio.on("submit vote")
+# def vote(data):
+# 	selection = data["selection"]
+# 	emit("anaunce vote", {"selection": selection}, broadcast=True)
+
+
+# if __name__ == '__main__':
+#     socketio.run(app)
+
+
+# End vote 0 section---
+
+votes = {"yes": 0, "no": 0, "maybe": 0}
+
 @app.route("/")
 def index():
-    return 'ok'
+    return render_template("index.html", votes = votes)
+
+
+@socketio.on("submit vote")
+def vote(data):
+	
+	selection = data["selection"] 
+	votes[selection] +=1
+
+	emit("vote totals", votes, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
